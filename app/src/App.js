468b052bf4ceda372be50bf0f48bc4edf3632b4a
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2023-02-04 18:59:31 -0300

    Criação de alguns componentes e estilos iniciais do App

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+
 function App() {
-    return <div>Página Inicial</div>;
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
---
+++ app/src/Components/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      Login / Criar
+    </div>
+  )
+}
+
+export default Login
