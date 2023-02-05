4493de3783ba136560d405bc5059cc4b765659eb
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2023-02-05 12:58:37 -0300

    Criação dos componentes de Login e suas rotas

--- app/src/Components/Login/Login.js
+++ app/src/Components/Login/Login.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LoginForm from "../LoginForm/LoginForm";
+import LoginCreate from "../LoginCreate/LoginCreate";
+import LoginPasswordLost from "../LoginPasswordLost/LoginPasswordLost";
+import LoginPasswordReset from "../LoginPasswordReset/LoginPasswordReset";
 
 const Login = () => {
-  return (
-    <div>
-      Login / Criar
-    </div>
-  )
-}
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<LoginForm />} />
+                <Route path="criar" element={<LoginCreate />} />
+                <Route path="perdeu" element={<LoginPasswordLost />} />
+                <Route path="resetar" element={<LoginPasswordReset />} />
+            </Routes>
+        </div>
+    );
+};
 
-export default Login
+export default Login;
