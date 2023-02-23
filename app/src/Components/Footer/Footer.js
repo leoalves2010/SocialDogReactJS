89af716d43e0ff07be9d9e00cb0ea89488b853de
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2023-02-23 20:15:37 -0300

    Estilização do footer

--- app/src/Components/Footer/Footer.js
+++ app/src/Components/Footer/Footer.js
@@ -1,8 +1,14 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import { ReactComponent as DogsFooter } from "../../Assets/dogs-footer.svg";
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer className={styles.footer}>
+            <DogsFooter />
+            <p>Social Dogs. Alguns direitos reservados.</p>
+        </footer>
+    );
 };
 
 export default Footer;
